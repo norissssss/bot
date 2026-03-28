e7d244a916f5f6898e4261a344182336f1a9904b
noriss <werqer2werqem3@gmail.com>
2026-03-28 21:25:49 +0300

    Update pingexample.py

--- pingexample.py
+++ pingexample.py
@@ -1,12 +1,13 @@
 from discord.ext import commands
+from discord import app_commands
 
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send("🏓 Pong!")
+    @app_commands.command(name="ping", description="Ping command")
+    async def ping(self, interaction):
+        await interaction.response.send_message("🏓 Pong!")
 
 async def setup(bot):
     await bot.add_cog(Ping(bot))
